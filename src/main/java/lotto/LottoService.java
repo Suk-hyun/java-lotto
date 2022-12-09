f656747c65a1f699ac59bd755dcd95ba4d429885
simba <shim9597@gmail.com>
2022-12-09 23:06:13 +0900

    feat: 한개의 로또의 등수를 구하는 기능 구현

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.domain.LottoPolicy;
+
 import java.util.List;
 
 public class LottoService {
@@ -8,9 +10,25 @@ public class LottoService {
         int result = 0;
         for (Integer winningNumber : winningNumbers) {
             if (lottoNumbers.contains(winningNumber)) {
-                result ++;
+                result++;
             }
         }
         return result;
+    }
+
+    public int classifyRank(int result, List<Integer> lottoNumbers, int bonusNum) {
+        if (result == 3) {
+            return LottoPolicy.FIFTH.rank();
+        }
+        if (result == 4) {
+            return LottoPolicy.FOURTH.rank();
+        }
+        if (result == 5) {
+            if (lottoNumbers.contains(bonusNum)) {
+                return LottoPolicy.SECOND.rank();
+            }
+            return LottoPolicy.THIRD.rank();
+        }
+        return LottoPolicy.FIRST.rank();
     }
 }
