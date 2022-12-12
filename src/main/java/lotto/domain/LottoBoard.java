ced84268c2a7a404c36f7d032539621c40239131
simba <shim9597@gmail.com>
2022-12-12 20:25:29 +0900

    feat: 로또 등수, 당첨 금액, 해당 등수의 당첨 로또 개수를 저장하는 객체 구현

---
+++ src/main/java/lotto/domain/LottoBoard.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+public enum LottoBoard {
+    FIRST(1, 0,2_000_000_000),
+    SECOND(2,  0,30_000_000),
+    THIRD(3,  0,1_500_000),
+    FOURTH(4,  0,50_000),
+    FIFTH(5,  0,5_000);
+
+    private final int rank;
+    private int score;
+
+    private final int winningMoney;
+
+    LottoBoard(int rank, int score, int winningMoney) {
+        this.rank = rank;
+        this.score = score;
+        this.winningMoney = winningMoney;
+    }
+
+    public int rank() {
+        return rank;
+    }
+
+    public int score() {
+        return score;
+    }
+    public void incScore() {
+        score ++;
+    }
+
+    public int winningMoney() {
+        return winningMoney;
+    }
+}
