0843163172f7efd98f2a5257b2b807ba99a5240b
simba <shim9597@gmail.com>
2023-05-16 22:18:11 +0900

    refactor: 검증 기능 추가

--- src/main/java/lotto/Validations.java
+++ src/main/java/lotto/Validations.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Validations {
 
     public static void validate(List<Integer> numbers) {
+        isValidRangeForList(numbers);
         isValidSize(numbers);
         isDuplicate(numbers);
     }
@@ -17,6 +18,18 @@ public class Validations {
 
     private static void isValidSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isValidRangeForList(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            isValidRange(number);
+        }
+    }
+
+    public static void isValidRange(Integer number) {
+        if (number < 1 || number > 45) {
             throw new IllegalArgumentException();
         }
     }
